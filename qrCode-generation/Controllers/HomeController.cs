fde2f83e712f09667d28b943ec4cea53a05ea206
k-muhibul <65398190+k-muhibul@users.noreply.github.com>
2021-02-10 14:11:06 +0600

    all

--- qrCode-generation/Controllers/HomeController.cs
+++ qrCode-generation/Controllers/HomeController.cs
@@ -14,11 +14,13 @@ using qrCode_generation.Models;
 using LumenWorks.Framework.IO.Csv;
 using System.Web.Mvc;
 using System.Data;
+using qrCode_generation.ViewModel;
 
 namespace qrCode_generation.Controllers
 {
     public class HomeController : Controller
     {
+        List<QRViewModel> qrvm = new List<QRViewModel>();
 
 
         public ActionResult Index()
@@ -27,35 +29,24 @@ namespace qrCode_generation.Controllers
 
 
         }
-        [HttpPost]
-        public ActionResult Index(HttpPostedFileBase file)
+        [HttpGet]
+        public ActionResult Upload()
         {
+            return View(qrvm);
+
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+           
             try
             {
                 
                 if (file != null && file.ContentLength > 0)
                 {
-                    PdfDocument doc = new PdfDocument();
-
-                    PdfPageBase page = doc.Pages.Add();
-                    PdfGrid grid = new PdfGrid();
-                    PdfGridRow row = grid.Rows.Add();
-                    row = grid.Rows.Add();
-                    float width = page.Canvas.ClientSize.Width - (grid.Columns.Count + 1);
-                    grid.Columns.Add(5);
-                    grid.Style.CellPadding = new PdfPaddings(1, 1, 1, 1);
-                    grid.Columns[0].Width = width * 0.20f;
-
-                    grid.Columns[1].Width = width * 0.20f;
-                    grid.Columns[2].Width = width * 0.20f;
-                    grid.Columns[3].Width = width * 0.20f;
-                    grid.Columns[4].Width = width * 0.20f;
-
-                    grid.Rows[0].Height = width * 0.15f;
-
-                    grid.Rows[1].Height = width * 0.15f;
-
-                    string outputPDFfile = "D:/qrPdf1.pdf";
 
                     var csvTable = new DataTable();
 
@@ -78,63 +69,36 @@ namespace qrCode_generation.Controllers
                             csvData.Add(csv[i]);
                             
                         }
-                        for (var l = 0; l < csvData.Count-2; l++)
-                        {
-
-                            if (l < csvData.Count - 3)
-                            {
-                                row = grid.Rows.Add();
-                                grid.Rows[l + 1].Height = grid.Rows[l].Height;
-                            }
-                            else grid.Rows[l].Height = grid.Rows[0].Height;
-                        }
+                      
                         int z, g;
-                        for (z = 0, g = 1; g < csvData.Count-1; z++,g++)
+                        for (z = 0, g = 1; g < csvData.Count; z++,g++)
                         {
                             QRCodeGenerator qrGenerator = new QRCodeGenerator();
                             
                             QRCodeData qrCodeData = qrGenerator.CreateQrCode(csvData[g], QRCodeGenerator.ECCLevel.Q);
                             QRCode qrCode = new QRCode(qrCodeData);
-                            Bitmap qrCodeImage = qrCode.GetGraphic(35);
+                            Bitmap qrCodeImage = qrCode.GetGraphic(100);
                             var codeBitmap = new Bitmap(qrCodeImage);
+                            var bitmaptobyte = BitmapToBytes(codeBitmap);
                             Image image = (Image)codeBitmap;
-                            PdfGridCellContentList lst = new PdfGridCellContentList();
-
-                            PdfGridCellContent textAndStyle = new PdfGridCellContent();
-                            PdfGridCellContent text = new PdfGridCellContent();
-                            PdfGridCellContent full = new PdfGridCellContent();
-                            textAndStyle.Image = PdfImage.FromImage(image);
-
-                            textAndStyle.ImageSize = new SizeF(65, 65);
-
                             var codeNo = csvData[g].Split(new string[] { "," }, StringSplitOptions.None);
-                            
-                            text.Text = "\n" + codeNo[1];
-               
-                            lst.List.Add(textAndStyle);
-                            lst.List.Add(text);
-                            for (var k = 0; k < 5; k++)
-                            {
-                                
-                                    grid.Rows[z].Cells[k].Value = lst;
-                                
-                            }
-                            PdfLayoutResult result = grid.Draw(page, new PointF(10, 30));
+                            QRViewModel qr = new QRViewModel();
 
+
+
+                            qr.Image = bitmaptobyte;
+                            qr.name = codeNo[0];
+                            qrvm.Add(qr);
 
                         }
-
-                        doc.SaveToFile(outputPDFfile, FileFormat.PDF);
-
-                        System.Diagnostics.Process.Start(outputPDFfile);
-
+                       
 
                     }
 
                 }
   
                 ViewBag.Message = "File Uploaded Successfully!!";
-                return View();
+                return View(qrvm);
 
 
             }
@@ -148,96 +112,19 @@ namespace qrCode_generation.Controllers
 
 
 
+        private static byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
 
 
 
 
-        //public ActionResult generateQrcode()
-        //{
-
-        //    PdfDocument doc = new PdfDocument();
-
-        //    PdfPageBase page = doc.Pages.Add();
-        //    PdfGrid grid = new PdfGrid();
-        //    PdfGridRow row = grid.Rows.Add();
-        //    row = grid.Rows.Add();
-        //    float width = page.Canvas.ClientSize.Width - (grid.Columns.Count + 1);
-        //    grid.Columns.Add(5);
-        //    grid.Style.CellPadding = new PdfPaddings(1, 1, 1, 1);
-        //    grid.Columns[0].Width = width * 0.20f;
-
-        //    grid.Columns[1].Width = width * 0.20f;
-        //    grid.Columns[2].Width = width * 0.20f;
-        //    grid.Columns[3].Width = width * 0.20f;
-        //    grid.Columns[4].Width = width * 0.20f;
-
-        //    grid.Rows[0].Height = width * 0.13f;
-
-        //    grid.Rows[1].Height = width * 0.13f;
-
-        //    string outputPDFfile = "D:/qrPdf.pdf";
 
 
-        //    var csvTable = new DataTable();
-        //    string rowInfo = "";
-
-        //    using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"D:/200data.csv")), true))
-        //    {
-        //        csvTable.Load(csvReader);
-        //        int total_row = csvTable.Rows.Count;
-
-        //        for (var l = 0; l < csvTable.Rows.Count; l++)
-        //        {
-
-        //            if (l < csvTable.Rows.Count - 1)
-        //            {
-        //                row = grid.Rows.Add();
-        //                grid.Rows[l + 1].Height = grid.Rows[l].Height;
-        //            }
-        //            else grid.Rows[l].Height = grid.Rows[0].Height;
-        //        }
-        //        for (var i = 0; i < csvTable.Rows.Count; i++)
-        //        {
-        //            rowInfo = "";
-
-        //            for (var j = 0; j < csvTable.Columns.Count; j++)
-        //            {
-        //                //ekta row er information->qrCode Conversion->printing on pdf
-        //                rowInfo = rowInfo + csvTable.Rows[i][j].ToString() + ",--";
-        //                QRCodeGenerator qrGenerator = new QRCodeGenerator();
-        //                QRCodeData qrCodeData = qrGenerator.CreateQrCode(rowInfo, QRCodeGenerator.ECCLevel.Q);
-        //                QRCode qrCode = new QRCode(qrCodeData);
-        //                Bitmap qrCodeImage = qrCode.GetGraphic(1);
-        //                var codeBitmap = new Bitmap(qrCodeImage);
-        //                Image image = (Image)codeBitmap;
-
-        //                PdfGridCellContentList lst = new PdfGridCellContentList();
-
-        //                PdfGridCellContent textAndStyle = new PdfGridCellContent();
-        //                textAndStyle.Image = PdfImage.FromImage(image);
-
-        //                textAndStyle.ImageSize = new SizeF(65, 65);
-
-        //                lst.List.Add(textAndStyle);
-        //                for (var k = 0; k < 5; k++)
-        //                {
-        //                    grid.Rows[i].Cells[k].Value = lst;
-
-        //                }
-        //                PdfLayoutResult result = grid.Draw(page, new PointF(10, 30));
-
-        //            }
-
-        //        }
-
-
-        //    }
-        //    doc.SaveToFile(outputPDFfile, FileFormat.PDF);
-
-        //    System.Diagnostics.Process.Start(outputPDFfile);
-        //    return View();
-        //}
-
-      
     }
 }
---
+++ qrCode-generation/ViewModel/QRViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace qrCode_generation.ViewModel
+{
+    public class QRViewModel
+    {
+
+
+     
+        public byte[] Image { get; set; }
+        public string name { get; set; }
+    }
+}
